7cb77b30a8a287c3eaba95d0cb239a14beb1e481
Poison <tianshuang.me@gmail.com>
2017-02-28 23:27:25 +0800

    read the oss configuration from the oss_config.json

--- incremental_upload_to_aliyun_oss.py
+++ incremental_upload_to_aliyun_oss.py
@@ -1,15 +1,10 @@
 import base64
 import hashlib
+import json
 import os
 
 import oss2
 import requests
-
-auth = oss2.Auth('AccessKeyId', 'AccessKeySecret')
-bucket = oss2.Bucket(auth, 'Endpoint', 'BucketName')
-oss_public_domain = 'OssDomain'
-
-local_dir = "/Users/Poison/blog/public/"
 
 
 def content_md5(local_file_path):
@@ -30,7 +25,7 @@ def upload_file_to_aliyun_oss(local_file_path):
     local_file_md5 = content_md5(local_file_path)
     exist = bucket.object_exists(oss_object_key)
     if exist:
-        oss_file_head = requests.head("https://" + oss_public_domain + "/" + oss_object_key)
+        oss_file_head = requests.head("https://" + oss_domain + "/" + oss_object_key)
         if local_file_md5 == oss_file_head.headers["Content-MD5"]:
             return
 
@@ -43,6 +38,29 @@ def upload_file_to_aliyun_oss(local_file_path):
 
 
 if __name__ == '__main__':
+    oss_config = None
+    with open('oss_config.json') as oss_config_file:
+        oss_config = json.load(oss_config_file)
+        if 'accessKeyId' not in oss_config:
+            raise ValueError("No accessKeyId in oss_config.json")
+        if 'accessKeySecret' not in oss_config:
+            raise ValueError("No accessKeySecret in oss_config.json")
+        if 'endpoint' not in oss_config:
+            raise ValueError("No endpoint in oss_config.json")
+        if 'bucketName' not in oss_config:
+            raise ValueError("No bucketName in oss_config.json")
+        if 'ossDomain' not in oss_config:
+            raise ValueError("No ossDomain in oss_config.json")
+        if 'localDir' not in oss_config:
+            raise ValueError("No localDir in oss_config.json")
+        if not str(oss_config['localDir']).strip().endswith('/'):
+            raise ValueError("localDir must end with a slash, example: /Users/Poison/blog/public/")
+
+    auth = oss2.Auth(oss_config['accessKeyId'], oss_config['accessKeySecret'])
+    bucket = oss2.Bucket(auth, oss_config['endpoint'], oss_config['bucketName'])
+    oss_domain = oss_config['ossDomain']
+    local_dir = oss_config['localDir']
+
     for dirpath, dirnames, filenames in os.walk(local_dir):
         for filename in filenames:
             upload_file_to_aliyun_oss(os.path.join(dirpath, filename))
