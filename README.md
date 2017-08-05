41116e0952a5f9fade3fbfa51f40730cf6cc8abd
Poison <tianshuang.me@gmail.com>
2017-08-05 10:12:02 +0800

    add multi-threaded support and optimization

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
 # aliyun-oss-sync
 
-阿里云 OSS 增量上传脚本
-
-此脚本是用来发布我个人博客 [Poison](https://tianshuang.me/) 而编写的，因为工作中常用语言为 Java，而 Python 仅是副业，代码如有不当之处，敬请指出。
+### 简介
+阿里云 OSS 增量上传脚本，此脚本是用来发布我个人博客 [Poison](https://tianshuang.me/) 而编写的，因为工作中常用语言为 Java，而 Python 仅是副业，代码如有不当之处，敬请指出。
 
 逻辑很简单，递归遍历本地目录，然后判断每个文件在 OSS 里是否存在，如果不存在则直接上传，如果存在则检查 Content-Md5 是否相等，如果不相等则表明该文件内容已经发生变化，则上传该文件，OSS 会自动覆盖同名文件。
 
-值得注意的是检查 Content-Md5 的值是用的 HTTP 的 HEAD 方法，因为我们只需要 header 中的 Content-Md5 字段的值，所以并不需要使用 GET 方法拿到响应体，这样既加快了速度也节省了 OSS 流量。
+值得注意的是检查 Content-Md5 的值是用的 HTTP 的 HEAD 方法，因为我们只需要 header 中的 Content-Md5 字段的值，所以并不需要使用 GET 方法拿到响应体，这样既加快了速度也节省了 OSS 流量。遍历是单个线程在进行遍历，然后将遍历到的文件上传任务放进了线程池，因为上传任务为 I/O 密集型，故使用多线程上传，采用 Python 指定的默认线程数。源代码如下：
+
+```python
+max_workers = (os.cpu_count() or 1) * 5
+```
 
 关于 ossDomain 的值，你如果在同地域内网的 ECS 上使用该脚本，建议使用内网域名，速度快并且节省了流量费用，否则使用外网域名。
 
-将配置信息填写到 oss_config.json 中即可使用。
+环境要求：
+Python 3.2 +
 
-#### TODO
-* 代码目前只支持 Python2，后续将支持 Python3
-* 当多数文件为小文件时，单线程上传可考虑优化为多线程上传，因为大多数时间在等待网络 IO，若上传大文件则无必要，因为上行速率有限
-* 对一些边界条件的处理，对 Object 的大小限制，命名限制进行检查
+使用方法：
+
+下载 [incremental_upload_to_aliyun_oss.py](https://storage.tianshuang.me/aliyun-oss-sync/incremental_upload_to_aliyun_oss.py)
+
+下载 [oss_config.json](https://storage.tianshuang.me/aliyun-oss-sync/oss_config.json)
+
+将你的 OSS 配置信息替换掉 oss_config.json 中的模版配置，并将以上两个文件放置于同一目录下即可运行。
+
+```Bash
+python3 incremental_upload_to_aliyun_oss.py
+```
+
+如有建议，敬请指出。
--- incremental_upload_to_aliyun_oss.py
+++ incremental_upload_to_aliyun_oss.py
@@ -2,41 +2,38 @@ import base64
 import hashlib
 import json
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 import oss2
 import requests
 
 
 def content_md5(local_file_path):
-    with open(local_file_path, 'rb') as fileobj:
+    with open(local_file_path, 'rb') as file:
         m = hashlib.md5()
         while True:
-            d = fileobj.read(8096)
+            d = file.read(8096)
             if not d:
                 break
             m.update(d)
-        return str(base64.b64encode(m.digest()))
+        return str(base64.b64encode(m.digest()), 'utf-8')
 
 
 def upload_file_to_aliyun_oss(local_file_path):
-    if local_file_path.endswith('.DS_Store'):
-        return
     if is_windows:
         local_file_path = local_file_path.replace('\\', '/')
+    if local_file_path.endswith('.DS_Store') or not os.path.isfile(local_file_path):
+        return
     oss_object_key = local_file_path[local_dir.__len__():]
-    local_file_md5 = content_md5(local_file_path)
-    exist = bucket.object_exists(oss_object_key)
-    if exist:
-        oss_file_head = requests.head('https://' + oss_domain + '/' + oss_object_key)
-        if local_file_md5 == oss_file_head.headers['Content-MD5']:
-            return
+    oss_response = requests.head('https://' + oss_domain + '/' + oss_object_key)
+    if oss_response.status_code == 200 and content_md5(local_file_path) == oss_response.headers['Content-MD5']:
+        return
 
-    with open(local_file_path, 'rb') as fileobj:
-        print 'Uploading: ' + local_file_path
-        result = bucket.put_object(oss_object_key, fileobj, headers={'Content-MD5': local_file_md5})
-        if result.status != 200:
-            print 'upload error, response information: ' + str(result)
-            exit(1)
+    print('uploading: ' + local_file_path)
+    result = bucket.put_object_from_file(oss_object_key, local_file_path)
+    if result.status != 200:
+        print('upload error, response information: ' + str(result))
+        exit(1)
 
 
 if __name__ == '__main__':
@@ -67,6 +64,7 @@ if __name__ == '__main__':
     oss_domain = oss_config['ossDomain']
     local_dir = oss_config['localDir']
 
-    for dirpath, dirnames, filenames in os.walk(local_dir):
-        for filename in filenames:
-            upload_file_to_aliyun_oss(os.path.join(dirpath, filename))
+    with ThreadPoolExecutor() as executor:
+        for dirpath, dirnames, filenames in os.walk(local_dir):
+            for filename in filenames:
+                executor.submit(upload_file_to_aliyun_oss, os.path.join(dirpath, filename))
